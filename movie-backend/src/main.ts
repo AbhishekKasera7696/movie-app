0772981fe9bdc2e1c9227ff33bc1651ad177a274
Abhishek Kasera <akasera62@gmail.com>
2025-11-17 14:17:57 +0530

    .

--- movie-backend/src/main.ts
+++ movie-backend/src/main.ts
@@ -4,13 +4,26 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const allowedOrigins = [
+    'http://localhost:3001',                
+    'https://movie-app-alpha-flax.vercel.app' 
+  ];
+
   app.enableCors({
-    origin: '*',           
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('CORS not allowed'));
+      }
+    },
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
-    credentials: true,
+    credentials: true, 
   });
 
   await app.listen(3000, '0.0.0.0');
+  console.log(`Backend running on http://0.0.0.0:3000`);
 }
 bootstrap();
