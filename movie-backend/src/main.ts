c6f505f8f343e4e2ed38b0b1a32a5c49b9ff8714
Abhishek Kasera <akasera62@gmail.com>
2025-11-17 14:06:18 +0530

    changes in jwt strategy

--- movie-backend/src/main.ts
+++ movie-backend/src/main.ts
@@ -4,16 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // ✅ CORS setup
   app.enableCors({
-    origin: [
-      'http://localhost:3001', 
-      'https://movie-app-alpha-flax.vercel.app' 
-    ],
+    origin: '*',           
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
   });
 
-  await app.listen(3000, '0.0.0.0'); 
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
